f0a12d57bf902e89c04379356de42bb9afe17d50
Guanya Peng <guanya.peng24@gmail.com>
2023-10-28 13:01:24 +0200

    update keybinding to delete trailing spaces

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
+-- options to be silent
+local all_silent = { noremap = true, silent = true }
+
+-- function to create keymap_options
+local function keymap_options(description, ...)
+  local options = { desc = description }
+  local args = { ... }
+  for _, arg in ipairs(args) do
+    if type(arg) == "table" then
+      for k, v in pairs(arg) do
+        options[k] = v
+      end
+    end
+  end
+  return options
+end
+
 -- Copy to clipboard in all mode
 vim.keymap.set({"n", "v"}, "<leader>y",'"+y')
 
@@ -12,13 +29,16 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
 
 -- Map Tab key to :bnext
-vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', { noremap = true, silent = true, desc = 'Next Buffer' })
+vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', keymap_options('Next Buffer', all_silent))
 
 -- Map Shift-Tab key to :bprev
-vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprev<CR>', { noremap = true, silent = true, desc = 'Previous Buffer' })
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprev<CR>',keymap_options('Previous Buffer', all_silent))
 
 -- Map Ctrl-w to close the current buffer
-vim.api.nvim_set_keymap('n', '<C-w>', ':bd<CR>', { noremap = true, silent = true, desc = 'Close current buffer'})
+vim.api.nvim_set_keymap('n', '<C-w>', ':bd<CR>', keymap_options('Close current buffer', all_silent))
 
 -- Map Ctrl-Shift-w to wipe out the current buffer
-vim.api.nvim_set_keymap('n', '<C-S-w>', ':bw<CR>', { noremap = true, silent = true, desc= 'Wipe out current buffer' })
+vim.api.nvim_set_keymap('n', '<C-S-w>', ':bw<CR>', keymap_options('Wipe out current buffer', all_silent))
+
+-- Delete trailing spaces in every line
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>x', ':silent! %s/\\s\\+$//e<CR>', keymap_options('Delete all trailing spaces', all_silent))
