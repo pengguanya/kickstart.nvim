bee31ba371ce1a82f5518bd8c2c04b33b8a3085b
Guanya Peng <guanya.peng24@gmail.com>
2023-10-30 18:37:52 +0100

    use generic setup for vim-slime instead of tvim specific

--- lua/custom/plugins/vim-slime.lua
+++ lua/custom/plugins/vim-slime.lua
@@ -3,7 +3,8 @@ return {
   -- Config for vim-slime
   config = function()
     vim.g.slime_target = 'tmux'
-    vim.g.slime_default_config = { socket_name = 'tvim_socket', target_pane = ':0.1' }
-    vim.g.slime_dont_ask_default = 1
+--    vim.g.slime_default_config = { socket_name = 'tvim_socket', target_pane = ':0.1' } -- To use tvim_socket for tvim
+      vim.g.slime_default_config = { socket_name = "default", target_pane = "{next}" } -- general setup
+      vim.g.slime_dont_ask_default = 1
   end,
 }
