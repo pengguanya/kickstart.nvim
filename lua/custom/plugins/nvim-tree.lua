281feb47309e34a1e9999d68b37f75586952fc37
Guanya Peng <guanya.peng24@gmail.com>
2023-10-30 18:37:05 +0100

    comment for netrw

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -5,6 +5,8 @@ return {
   }, -- optional, for file icons
   config = function()
     require('nvim-tree').setup({
+       -- disable_netrw = false,     -- uncomment this line to enable netrw 
+       -- hijack_netrw = true,       -- uncomment this line to enable netrw 
        sort_by = "case_sensitive",
        renderer = {
          group_empty = true,
