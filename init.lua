6399b4ad662a2b7830251457b3a9bba87b806286
Guanya Peng <guanya.peng24@gmail.com>
2024-06-22 17:11:00 +0200

    update nvim

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -778,16 +778,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      --   -- Load the colorscheme here.
+      --   -- Like many other themes, this one has different styles, and you could load
+      --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'gruvbox'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      --   -- You can configure highlights by doing something like:
+      --   vim.cmd.hi 'Comment gui=none'
     end,
   },
 
@@ -877,7 +877,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
@@ -885,7 +885,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -908,5 +908,7 @@ require('lazy').setup({
   },
 })
 
+require 'config'
+--
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,7 @@ local function keymap_options(description, ...)
   local options = { desc = description }
   local args = { ... }
   for _, arg in ipairs(args) do
-    if type(arg) == "table" then
+    if type(arg) == 'table' then
       for k, v in pairs(arg) do
         options[k] = v
       end
@@ -16,23 +16,23 @@ local function keymap_options(description, ...)
 end
 
 -- Copy to clipboard in all mode
-vim.keymap.set({"n", "v"}, "<leader>y",'"+y')
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
 
 -- Resize window using <ctrl> arrow keys
-vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase window height' })
+vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease window height' })
+vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease window width' })
+vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase window width' })
 
 -- additional telescope keymap (default ones already in init.lua)
-local builtin = require('telescope.builtin')
+local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
 
 -- Map Tab key to :bnext
 vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', keymap_options('Next Buffer', all_silent))
 
 -- Map Shift-Tab key to :bprev
-vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprev<CR>',keymap_options('Previous Buffer', all_silent))
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprev<CR>', keymap_options('Previous Buffer', all_silent))
 
 -- Map Ctrl-w to close the current buffer
 vim.api.nvim_set_keymap('n', '<C-w>', ':bd<CR>', keymap_options('Close current buffer', all_silent))
@@ -43,9 +43,11 @@ vim.api.nvim_set_keymap('n', '<C-S-w>', ':bw<CR>', keymap_options('Wipe out curr
 -- Delete trailing spaces in every line
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>x', ':silent! %s/\\s\\+$//e<CR>', keymap_options('Delete all trailing spaces', all_silent))
 
-
--- [[ User defined functions ]] 
-local utils = require("config.functions") 
+-- [[ User defined functions ]]
+local utils = require 'config.functions'
 
 -- Switch between camel and sanke case
 vim.keymap.set('n', '<Leader>k', utils.switch_case, keymap_options('Switch word under cursor from CamMel to snake_case', all_silent))
+
+--- Toggle NeoTree
+vim.keymap.set('n', '<leader>g', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle NeoTree' })
