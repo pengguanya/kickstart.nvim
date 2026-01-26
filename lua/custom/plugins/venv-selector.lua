8854d1ec1e940c2af155872de94eec95fe438986
Guanya Peng <guanya.peng24@gmail.com>
2026-01-26 22:00:16 +0100

    chore: update colorscheme and plugin configs

--- lua/custom/plugins/venv-selector.lua
+++ lua/custom/plugins/venv-selector.lua
@@ -2,16 +2,14 @@ return {
   'linux-cultist/venv-selector.nvim',
   dependencies = {
     'neovim/nvim-lspconfig',
-    'mfussenegger/nvim-dap',
-    'mfussenegger/nvim-dap-python', --optional
-    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
   },
-  lazy = false,
-  branch = 'regexp', -- This is the regexp branch, use this for the new version
-  config = function()
-    require('venv-selector').setup()
-  end,
+  ft = 'python', -- Load when opening Python files
   keys = {
-    { ',v', '<cmd>VenvSelect<cr>' },
+    { ',v', '<cmd>VenvSelect<cr>' }, -- Open picker on keymap
+  },
+  opts = { -- this can be an empty lua table - just showing below for clarity.
+    search = {}, -- if you add your own searches, they go here.
+    options = {}, -- if you add plugin options, they go here.
   },
 }
